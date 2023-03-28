843a0d94734b7c3e9f162eb5cc02909fb5b0c3dd
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-03-28 18:33:40 +0530

    Mobile responsiveness still pending

---
+++ myapp/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+import { BsThreeDotsVertical, BsSearch } from "react-icons/bs";
+import {  FaTimes } from "react-icons/fa";
+import "../../Styles/main.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>SATVIKA</h3>
+			<nav ref={navRef}>
+        
+				<a href="/#">Home</a>
+				<a href="/#">About Us</a>
+				<a href="/#">Portfolio</a>
+				<a href="/#">Active Project</a>
+                <a href="/#">Services</a>
+                <a href="/#">Latest News</a>
+                <a  href="/#">Tools</a>
+               
+                <button
+                 className="contact--us">Contact</button>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+            
+			</nav>
+            <button
+            className="nav-search nav-close-btn"
+            onClick={showNavbar}
+            >
+            <BsSearch/>
+        </button>
+			<button
+            
+				className="nav-btn"
+				onClick={showNavbar}>
+                
+				<BsThreeDotsVertical />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
