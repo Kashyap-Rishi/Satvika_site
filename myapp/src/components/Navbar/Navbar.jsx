50aa59f84472c04e8af1b531c6591dcb7e81937d
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-03-28 20:32:50 +0530

    Responsive carousel done

--- myapp/src/components/Navbar/Navbar.jsx
+++ myapp/src/components/Navbar/Navbar.jsx
@@ -2,53 +2,60 @@ import { useRef } from "react";
 import { BsThreeDotsVertical, BsSearch } from "react-icons/bs";
 import {  FaTimes } from "react-icons/fa";
 import "../../Styles/main.css";
+import Carousel from '../Carousel/Carousel';
+
 
 function Navbar() {
-	const navRef = useRef();
+    const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+    const showNavbar = () => {
+        navRef.current.classList.toggle(
+            "responsive_nav"
+        );
+    };
 
-	return (
-		<header>
-			<h3>SATVIKA</h3>
-			<nav ref={navRef}>
+    return (
+        <>
+        <header>
+            <h3>SATVIKA</h3>
+            <nav ref={navRef}>
         
-				<a href="/#">Home</a>
-				<a href="/#">About Us</a>
-				<a href="/#">Portfolio</a>
-				<a href="/#">Active Project</a>
+                <a href="/#">Home</a>
+                <a href="/#">About Us</a>
+                <a href="/#">Portfolio</a>
+                <a href="/#">Active Project</a>
                 <a href="/#">Services</a>
                 <a href="/#">Latest News</a>
                 <a  href="/#">Tools</a>
                
                 <button
                  className="contact--us">Contact</button>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
+                <button
+                    className="nav-btn nav-close-btn"
+                    onClick={showNavbar}>
+                    <FaTimes />
+                </button>
             
-			</nav>
+            </nav>
             <button
             className="nav-search nav-close-btn"
             onClick={showNavbar}
             >
             <BsSearch/>
         </button>
-			<button
+            <button
             
-				className="nav-btn"
-				onClick={showNavbar}>
+                className="nav-btn"
+                onClick={showNavbar}>
                 
-				<BsThreeDotsVertical />
-			</button>
-		</header>
-	);
+                <BsThreeDotsVertical />
+            </button>
+        </header>
+        <div className="hide-carousel">
+        <Carousel/>
+        </div>
+        </>
+    );
 }
 
 export default Navbar;
